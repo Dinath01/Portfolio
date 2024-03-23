203616c2941b6be220e9d0127bf99201a73a6dcd
Dinath01 <dinithnethmina101@gmail.com>
2024-03-23 12:21:23 +0530

    Base Template Adjustment II

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import backgroundImage from './bg.png';
 import './App.css';
-import userPicture from './User.png';
 
 const Portfolio = () => {
   return (
-    <div className="portfolio-container">
-      <div className="info">
-        <h2>Hello,</h2>
-        <h1>I'm Dinith Nethmina Surendra</h1>
-        <p>Undergraduate</p>
-      </div>
-      <div className="image">
-        <img src={userPicture} alt="User" />
+    <div>
+      <img className="background-image" src={backgroundImage} alt="Background" />
+      <div className="portfolio-container">
+        <div className="info">
+          <h2>Hello,</h2>
+          <h1>I'm Dinith Nethmina Surendra</h1>
+          <p>Undergraduate</p>
+        </div>
       </div>
     </div>
   );
